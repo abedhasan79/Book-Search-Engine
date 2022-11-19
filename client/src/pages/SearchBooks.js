35d518ccdb684b83ce0034632c81e54b988e0fdd
Abed Hasan <abedhasan79@gmail.com>
2022-11-19 10:14:35 -0500

    updated searchBook and SavedBooj js files

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+// import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
+import {useMutation} from '@apollo/client';
+import {SAVE_BOOK} from '../utils/mutations'
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -20,6 +21,9 @@ const SearchBooks = () => {
     return () => saveBookIds(savedBookIds);
   });
 
+
+  const saveBook = useMutation(SAVE_BOOK);
+
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -29,7 +33,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
