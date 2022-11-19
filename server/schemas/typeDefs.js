59de4a378eabc7b9cd652ea5807bb82b92cea5e4
Abed Hasan <abedhasan79@gmail.com>
2022-11-19 01:50:11 -0500

    updated typeDefs and resolver

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,7 +10,7 @@ const typeDefs = gql`
     }
 
     type Book{
-        bookId: String
+        bookId: String!
         authors: [String]
         description: String
         title:String!
@@ -22,26 +22,25 @@ const typeDefs = gql`
         token: ID!
         user: User
     }
-
+    input BookInput{
+        authors:[String]
+        description:String
+        title:String!
+        bookId:String!
+        image:String
+        link:String    
+    }
     type Query{
         me:User
     }
 
-    input BookInput{
-        authors:[String],
-        description:String,
-        title:String!,
-        bookId:String!,
-        image:String,
-        link:String    
-    }
 
-    type Mutation {
-        login(email:String!, password:String!):Auth
-        addUser(username:String!, email:String!, password: String!):Auth
-        saveBook(input: BookInput):User
-        removeBook:(bookId:ID!):User
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: String!): User
+  }
 `;
 
 module.exports = typeDefs;
